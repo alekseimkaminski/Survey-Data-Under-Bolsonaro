30b5a75629f70f90a1d0ecb763e134f6e165450e
Aleksei <68439753+alekseimkaminski@users.noreply.github.com>
2021-11-07 16:43:16 -0600

    Update and rename Simple Code.R to Convert SAV to CSV.R

---
+++ Convert SAV to CSV.R
@@ -0,0 +1,14 @@
+##Code for converting SAV files from SPSS into CSV files. 
+##CSV files are more compatible and readable when conducting data analysis in 
+##R and Excel or any program of your interest.
+
+library(foreign)
+
+whateverfileyouwanttocallit <- read.spss(file.choose(),to.data.frame=TRUE) #choose any file with open directory 
+
+View(whateverfileyouwanttocallit) 
+
+write.csv(whateverfileyouwanttocallit, file=newfile.csv")
+
+View(newfile) #sanity check to make sure file looks clean
+
